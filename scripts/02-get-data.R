eaf568aea3854fe5c25cd2f67a7160f235f11803
ecophilina <paenglis@lakeheadu.ca>
2020-06-04 13:25:08 -0700

    adding additional herring variables; make spp list for time period matching

--- scripts/02-get-data.R
+++ scripts/02-get-data.R
@@ -91,6 +91,8 @@ old_dat <- old_surv_all %>%
 
 old_dat$Date <- ymd(old_dat$Date)
 
+saveRDS(old_dat, "data/bird-survey-data-90s.rds")
+
 best_coords <- old_dat %>% 
   select(site, easting, northing, LengthM, SWidthM, AreaM2) %>% unique()
 
@@ -120,20 +122,24 @@ new_dat_coords <- left_join(new_dat, best_coords) %>% unique() %>%
 # sort(unique(old_dat$site))
 # sort(unique(new_dat$site))
 
+old_dat <- readRDS("data/bird-survey-data-90s.rds")
+
 dat <- bind_rows(old_dat, new_dat_coords) 
 
 View(dat)
 
 ### add in herring observations from 
 new_meta <- readxl::read_excel(
-  "raw/Coastal_Waterbird_Herring_Counts_2015&2016_5Feb2017.xlsx",
+  "raw/Coastal_Waterbird_Herring_Counts_2015&2016_1Jun2020.xlsx",
   sheet = "Survey_Summary"
 )
 # unique(new_meta$Surveyor)
 new_meta <- new_meta %>% select(
   Location, Event, Year, Month, Day, StartHR, StartMIN, ENDHR, ENDMIN, 
   `General visibility`, Vis_comment, Precip, WindSpeed, Tide, 
-  Disturbance, Boats, `Herring comments`, Comments 
+  Disturbance, Boats, 
+  Herring, Fishing, Milt, Eggs, Spawn_near, Active_spawn, Other_baitballs,
+  `Herring comments`, Comments 
   )
 
 
@@ -166,4 +172,22 @@ all_dat <- left_join(dat, new_meta2, by = c("year", "month", "day", "site")) #%>
 
 # View(all_dat)
  
+### make list of species with total records for each period
+# species_list <- all_dat %>% 
+#   group_by(SpeciesID, period) %>% 
+#   mutate(period_count = sum (SPCount)) %>% 
+#   select(SpeciesID, period, period_count) %>% unique() %>%
+#   pivot_wider(names_prefix = "period", names_from = period, values_from = period_count)
+# 
+# write.csv(species_list, "data/new-species-list.csv")
+
+### after groupings added manually, reimport
+spp_list <- read_csv("species-list.csv")
+all_dat <- left_join(all_dat, spp_list)
+
 saveRDS(all_dat, "data/bird-survey-data.rds")
+
+#### DFO DATA ####
+
+dfodat <- read_csv("raw/SpawnRaw-142.csv")
+
